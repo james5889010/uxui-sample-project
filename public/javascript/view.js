23e55d7dac28429d243b83e2e57080863f5f0dcf
UI UX 2016 <UI UX 2016>
2016-06-21 11:00:33 +0100

    setup gulp to compile sass and minify. Also setup gulp watch to run gulp on save

--- public/javascript/view.js
+++ public/javascript/view.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	function onGetRestaurantsSuccess(data) {
 
-		let dispList = document.getElementById("restaurants-list");
+		var dispList = document.getElementById("restaurants-list");
 
-		let restaurants = JSON.parse(data);
-		for (let i = 0 ; i < restaurants.length ; i++) {
-			let name = restaurants[0].RestaurantDescription.Name;
+		var restaurants = JSON.parse(data);
+		for (var i = 0 ; i < restaurants.length ; i++) {
+			var name = restaurants[0].RestaurantDescription.Name;
 
-			let liNode = document.createElement("li");
+			var liNode = document.createElement("li");
 			liNode.appendChild(document.createTextNode(name));
 			dispList.appendChild(liNode);
 		}
